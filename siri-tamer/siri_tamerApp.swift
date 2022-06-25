26629b17ef77b0dbc24d97cee16492cb6bae5a26
shota-nishizawa <guiltaugusthird03@gmail.com>
2022-06-25 23:40:15 +0900

    Create Empty xcode.proj

---
+++ siri-tamer/siri_tamerApp.swift
@@ -0,0 +1,17 @@
+//
+//  siri_tamerApp.swift
+//  siri-tamer
+//
+//  Created by shota-nishizawa on 2022/06/25.
+//
+
+import SwiftUI
+
+@main
+struct siri_tamerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
